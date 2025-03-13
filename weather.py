906839a653162fc41bf3c46b6c752448214b0960
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2025-03-12 22:10:21 -0400

    expand try block in weather

--- weather.py
+++ weather.py
@@ -5,11 +5,11 @@ import colorcet as cc
 def get_weather(api_key, zip) -> dict[str, float | str]:
     url = f"http://api.openweathermap.org/geo/1.0/zip?zip={zip}&appid={api_key}"
     # print(url)
-    response = requests.get(url)
-    res = response.json()
-    print(res)
-    url = f"http://api.openweathermap.org/data/3.0/onecall?lat={res['lat']}&lon={res['lon']}&exclude=hourly&units=imperial&appid={api_key}"
     try:
+        response = requests.get(url)
+        res = response.json()
+        print(res)
+        url = f"http://api.openweathermap.org/data/3.0/onecall?lat={res['lat']}&lon={res['lon']}&exclude=hourly&units=imperial&appid={api_key}"
         response = requests.get(url)
         if response.status_code == 200:
             res = response.json()
